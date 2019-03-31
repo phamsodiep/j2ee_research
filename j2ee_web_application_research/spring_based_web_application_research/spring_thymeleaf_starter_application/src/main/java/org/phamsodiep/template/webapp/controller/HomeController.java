235b9f1f3cbe3b17b68448ad8989d133f943c2e7
phamsodiep <phamduchuy@hotmail.com>
2019-03-30 21:57:29 -0700

    [j2ee_web_application_research/spring_based_web_application_research] add starter project 'spring_thymeleaf_starter_application' based on springboot framework with thymeleaf dependency imported

---
+++ j2ee_web_application_research/spring_based_web_application_research/spring_thymeleaf_starter_application/src/main/java/org/phamsodiep/template/webapp/controller/HomeController.java
@@ -0,0 +1,71 @@
+package org.phamsodiep.template.webapp.controller;
+
+
+import java.util.Set;
+import java.util.HashSet;
+import java.lang.annotation.Annotation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.config.BeanDefinition;
+
+import org.phamsodiep.template.webapp.model.ViewLink;
+import org.phamsodiep.template.webapp.stereotypes.Advertise;
+
+
+@Controller
+public class HomeController {
+  private static final String CONTROLLER_PACKAGE_NAME;
+  private static final String HOME_CONTROLLER_CLASS;
+
+  @GetMapping("/")
+  public String homePage(Model model) {
+    Set<ViewLink> links = scanControllerAdvertise();
+    model.addAttribute("links", links);
+    return "index";
+  }
+
+  private Set<ViewLink> scanControllerAdvertise() {
+    final String packageName = CONTROLLER_PACKAGE_NAME;
+    Set<ViewLink> result = new HashSet<ViewLink>();
+    ClassPathScanningCandidateComponentProvider scanner =
+      new ClassPathScanningCandidateComponentProvider(true);
+    scanner.addIncludeFilter(new AnnotationTypeFilter(Controller.class));
+    Set<BeanDefinition> bds = scanner.findCandidateComponents(packageName);
+
+    for (BeanDefinition bd : bds) {
+      String foundClassName = bd.getBeanClassName();
+      try {
+        if (foundClassName.compareTo(HOME_CONTROLLER_CLASS) != 0) {
+          Class<?> foundClass = Class.forName(foundClassName);
+          for (Annotation at : foundClass.getAnnotations()) {
+            if (at.annotationType() == Advertise.class) {
+              Advertise advertise = (Advertise) at;
+              ViewLink link = new ViewLink();
+              link.setPath(advertise.path());
+              link.setName(advertise.name());
+              result.add(link);
+              break;
+            }
+          }
+        }
+      }
+      catch(Exception e) {
+        //e.printStackTrace();
+      }
+    }
+
+    return result;
+  }
+
+  static {
+    CONTROLLER_PACKAGE_NAME = HomeController.class.getPackage().getName();
+    HOME_CONTROLLER_CLASS = HomeController.class.getName();
+  }
+}
+
+
---
+++ j2ee_web_application_research/spring_based_web_application_research/spring_thymeleaf_starter_application/src/main/java/org/phamsodiep/template/webapp/model/ViewLink.java
@@ -0,0 +1,26 @@
+package org.phamsodiep.template.webapp.model;
+
+
+public class ViewLink {
+  private String path;
+  private String name;
+
+
+  public String getPath() {
+    return this.path;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
+
+
