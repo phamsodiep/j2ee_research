c9d77bebd900e8483e0c836f1367f41f89888123
範疏葉 <37242476+phamsodiep@users.noreply.github.com>
2019-04-26 12:18:49 -0700

    [j2ee_web_application_research/spring_thymeleaf_starter_application][add patch] use wget to download file 'diep-org-chart.js' from orginal repository with LICENSE notice instead of cloning and embedded LICENSE notice

--- j2ee_web_application_research/spring_based_web_application_research/spring_thymeleaf_starter_application/upgrade/import_scripts/import_class_hierarchy_inspector_prj_v01.sh
+++ j2ee_web_application_research/spring_based_web_application_research/spring_thymeleaf_starter_application/upgrade/import_scripts/import_class_hierarchy_inspector_prj_v01.sh
@@ -4,3 +4,12 @@ cp -r upgrade/inspectors/00/class_hierarchy/src .
 cp -r upgrade/inspectors/01/class_hierarchy/src .
 cp upgrade/inspectors/start_server.sh .
 rm src/main/resources/templates/inspectors/showdownloadpage.html
+if [ ! -d ./src/main/resources/static/inspectors ]; then
+  mkdir src/main/resources/static/inspectors
+fi
+wget -O src/main/resources/static/inspectors/diep-org-chart.js https://github.com/phamsodiep/frontend_research/raw/master/angularjs/component/org_chart/jslib/diep-org-chart.js
+echo " "
+echo "Below is LICENSE notice for 'diep-org-chart.js' library"
+wget -q -O /dev/stdout https://github.com/phamsodiep/frontend_research/raw/master/LICENSE
+echo " "
+echo "Above is LICENSE notice for 'diep-org-chart.js' library"
